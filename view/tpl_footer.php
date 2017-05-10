14737660715b4a5e630605f8bb7fab48c2392021
Andrey Galushko <createsite2016@gmail.com>
2017-05-10 11:31:52 +0300

    Удалил старый JQ
    заменил JQ на свой.

--- view/tpl_footer.php
+++ view/tpl_footer.php
@@ -26,9 +26,6 @@
 
 </footer><!--/Футер-->
 
-
-
-<script src="js/jquery.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/jquery.scrollUp.min.js"></script>
 <script src="js/price-range.js"></script>
