17ac90d3222f973d34531c17081bb7940ddac1b5
Andrey Galushko <createsite2016@gmail.com>
2017-06-08 18:21:06 +0300

    Не помню что сделал..

--- admin/users.php
+++ admin/users.php
@@ -56,32 +56,23 @@ $i = 0;
 include('showdata_forpeople.php');
 $sql_get_device = $pdo->getRows("SELECT * FROM `users_8897532` ");
 foreach ( $sql_get_device as $data_get_device ) {
-$i++;
-?>
+    $i++; ?>
                   <tr>
-                    <td>
-<?php echo $data_get_device['profes'];// должность ?>
-                    </td>
-                    <td>
-<?php echo $data_get_device['name'];// проффесия ?>
-                    </td>
-                    <td>
-<?php echo $data_get_device['login']; // логин ?>
-                    </td>
-                    <td>
-<?php echo $data_get_device['password'];// пароль ?>
-                    </td>
-<td>
-<div class="btn-group">
-<a href="" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-pencil"></i></a>
-<ul class="dropdown-menu pull-right">
-<li><a href="fl_izm_user.php?id=<?php echo $data_get_device['id']; ?>">Ред-ть данные</a></li>
-<li class="divider"></li>
-<li class="divider"></li>
-<li><a href="classes/App.php?action=del_user&id=<?php echo $data_get_device['id']; ?>"><font color="red">Уволить сотрудника</font></a></li>
-</ul>
-</div>
-</td>
+                    <td><?php echo $data_get_device['profes'];// должность ?></td>
+                    <td><?php echo $data_get_device['name'];// проффесия ?></td>
+                    <td><?php echo $data_get_device['login']; // логин ?></td>
+                    <td><?php echo $data_get_device['password'];// пароль ?></td>
+                      <td>
+                          <div class="btn-group">
+                              <a href="" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-pencil"></i></a>
+                              <ul class="dropdown-menu pull-right">
+                                  <li><a href="fl_izm_user.php?id=<?php echo $data_get_device['id']; ?>">Ред-ть данные</a></li>
+                                  <li class="divider"></li>
+                                  <li class="divider"></li>
+                                  <li><a href="classes/App.php?action=del_user&id=<?php echo $data_get_device['id']; ?>"><font color="red">Уволить сотрудника</font></a></li>
+                              </ul>
+                          </div>
+                      </td>
                   </tr>
 <?php } ?>
 
@@ -91,20 +82,12 @@ $i++;
           </section>
         </div>
 
-
-
-
-
-
-
   </div>
-  <!-- / .modal -->
-  <!-- footer -->
   <br>
   <br>
   <br>
   <br>
-    <br>
+  <br>
   <br>
   <br>
   <br>
