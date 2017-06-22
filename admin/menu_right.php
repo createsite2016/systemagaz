96c49b015f2d8ea9e0b506aa3d81ded215c4912a
Andrey Galushko <createsite2016@gmail.com>
2017-06-22 07:23:41 +0300

    Меню справа
    Добавил новый пункт меню СТРАНИЦЫ, для управлением страницами магазина

--- admin/menu_right.php
+++ admin/menu_right.php
@@ -3,7 +3,8 @@ if ($user_role == 3)
 {
 ?>
           <li><a href="users.php"><i class="icon-group"></i> Сотрудники</a></li>
-          <li><a href="magazins.php"><i class="icon-cogs"></i>Настройки</a></li>
+          <li><a href="magazins.php"><i class="icon-cogs"></i> Настройки</a></li>
+          <li><a href="pages.php"><i class="icon-file"></i> Страницы</a></li>
           <li class="divider"></li>
           <li><a href="exit.php"><i class="icon-off"></i> Выйти</a></li>
 <?php } ?>
