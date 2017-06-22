2804c7c7a658023b6812e31d9c2e01aa999accc4
Andrey Galushko <createsite2016@gmail.com>
2017-06-22 07:21:11 +0300

    Контакты
    Теперь контакты, выгружаются из базы

--- contact.php
+++ contact.php
@@ -16,8 +16,8 @@ include_once('view/tpl_head.php');
                     <h2 class="title text-center">Контакты</h2>
                     <p>Мы всегда онлайн, пишите в любой удобный для Вас мессанджер, будем рады общению с Вами:</p>
                     <br>
-                        <p><a href="tel:+79649001435">Viber: +79649001435 <img src="images/viber.png" width="48"></p>
-                        <p><a href="tel:+79649001435">WhatsApp: +79649001435 <img src="images/whatsapp.png" width="48"></p>
+                        <p><a href="tel:<?php echo $shop['phone']; ?>">Viber: <?php echo $shop['phone']; ?> <img src="images/viber.png" width="48"></p>
+                        <p><a href="tel:<?php echo $shop['phone']; ?>">WhatsApp: <?php echo $shop['phone']; ?> <img src="images/whatsapp.png" width="48"></p>
                     <p><b><a target="_blank" href="https://t.me/xakerfsb">Telegram <img src="images/email.png" width="48"></a></b></p>
                     <p><b><a target="_blank" href="https://www.instagram.com/<?php echo $shop['instagram_login']; ?>">Instagram <img src="images/instagram.png" width="48"></a></b></p>
                     <p><b><a target="_blank" href="https://ok.ru/group/<?php echo $shop['id_ok_group']; ?>">Одноклассники <img src="images/odnoklassniki.png" width="48"></a></b></p>
