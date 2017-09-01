bde0f137caaecc62fbbf69adb12fbdd8f170cf87
Andrey Galushko <createsite2016@gmail.com>
2017-09-01 14:59:56 +0300

    Отправка теперь всего 1 запрос
    Теперь при отправке количества и ключей товара формируется массив и
    отправляется один запросом

--- js/eshop.js
+++ js/eshop.js
@@ -143,7 +143,7 @@ function checkCart() {
     //проверяю наличие корзины в localStorage
     if ( localStorage.getItem('cart') != null ) {
        cart = JSON.parse(localStorage.getItem('cart'));
-}
+    }
 }
 
 function showMiniCart() {
@@ -169,20 +169,34 @@ function showCart() {
     if ( rexdiv != undefined ) {
         // если есть элемент корзины, тогда прорисовываем ее
         rexdiv.innerHTML = out;
+        var ids = [];
+        var count = [];
+        var i = 0;
         console.log('===== корзина начало ======== ');
         for (var z in cart) {
-        console.log('отправка запроса на сервер с параметрами id:',z,' Количество:',cart[z]);
-            $.ajax({
-                url: '../ajax_cart.php', // на этот скрипт отправка
-                type: 'post', // метод post
-                response:'text',
-                data: {id: z, kolvo: cart[z]}, // параметры отправки
-                success: function (data) { // выполнение отправки
-                    console.log('заполенение переменной out для прорисовки',data);
-                    out += data;
-                    rexdiv.innerHTML = out;
-                }
-            });
+            if (cart[z] > 0 ) {
+                ids.push(z);
+                count.push(cart[z]);
+                console.log('отправка запроса на сервер с параметрами id:',z,' Количество:',cart[z]);
+                i++;
+
+            }
         }
+        //console.log('Ключи :',ids);
+        //console.log('Количество :',count);
+        $.ajax({
+            url: '../ajax_cart.php', // на этот скрипт отправка
+            type: 'post', // метод post
+            response:'text',
+            data: {id: ids, kolvo: count}, // параметры отправки
+            success: function (data) { // выполнение отправки
+                console.log('заполенение переменной out для прорисовки',data);
+                out += data;
+                rexdiv.innerHTML = out;
+                if (data == 'Нет ниодного товара в корзине') {
+                    document.getElementById('ndsfnsdfjsklf').style.display = 'none';
+                }
+            }
+        });
     }
 }
