47073e5c086bddbd4efcc48bc354a6f645ef562b
Andrey Galushko <createsite2016@gmail.com>
2017-09-01 14:58:38 +0300

    Добавил jquery
    Так требуется для библиотеки маски телефона

--- view/tpl_footer.php
+++ view/tpl_footer.php
@@ -34,6 +34,7 @@
 
 <script src="js/bootstrap.min.js"></script>
 <script src="js/jquery.scrollUp.min.js"></script>
+<script src="js/jquery.js"></script>
 <script src="js/price-range.js"></script>
 <script src="js/jquery.prettyPhoto.js"></script>
 <script src="js/main.js"></script>
