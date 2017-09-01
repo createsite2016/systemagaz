8aef48f859b369db1e8da3b295ff9e03408267ff
Andrey Galushko <createsite2016@gmail.com>
2017-09-01 14:53:55 +0300

    Переделал проверку остатков
    Теперь идет не множество запросов и проверок, сейчас формируется массив
    из всех параметров и делается один всего лишь запрос и один цикл
    обработки

--- ajax_cart.php
+++ ajax_cart.php
@@ -7,46 +7,44 @@ $kolvo_ = $_POST['kolvo_'];
 include_once "admin/classes/App.php"; // подключаем функции приложения
 $pdo = new Database();
 
-$sql_tovar = $pdo->getRow("SELECT * FROM `tovar` WHERE id = ? ",[$id_]); // получение данных о товаре
-if ( $kolvo_ < $sql_tovar['kolvo'] ) { // проверяем чтобы количество было больше нуля
-        echo $sql_tovar['kolvo'];
-    }
-if ( $kolvo_ >= $sql_tovar['kolvo'] ) { // проверяем чтобы количество было больше нуля
-        echo $sql_tovar['kolvo'];
-    }
+$i = 0;
+foreach ($id as $item) {
+    if ( $kolvo[$i] > '0' ) {
+    $sql_tovar = $pdo->getRow("SELECT * FROM `tovar` WHERE id = ? ",[$item]); // получение данных о товаре
 
-
-
-if ( $kolvo > '0' ) {
-    $sql_tovar = $pdo->getRow("SELECT * FROM `tovar` WHERE id = ? ",[$id]); // получение данных о товаре
-
-    if ( $kolvo > $sql_tovar['kolvo'] ) {
+    if ( $kolvo[$i] > $sql_tovar['kolvo'] ) {
         $out = "<tr><td>".$sql_tovar['id']."</td><td>";
         $out = $out."<a href=product.php?id=".$sql_tovar['id'].">".$sql_tovar['name'];
         $out = $out."</a></td><td>".$sql_tovar['chena_output'];
         $out = $out."</td><td><button onclick='minusCart(".$sql_tovar['id'].")' type='submit'>-</button>".$sql_tovar['kolvo'];
         $out = $out."шт.</td>";
-        $out = $out."<td><button onclick='removeCart(".$sql_tovar['id'].",".$kolvo.")' type='submit'>Убрать</button></td></tr>";
+        $out = $out."<td><button onclick='removeCart(".$sql_tovar['id'].",".$kolvo[$i].")' type='submit'>Убрать</button></td></tr>";
         echo $out;
     }
 
-    if ( $kolvo == $sql_tovar['kolvo'] ) {
+    if ( $kolvo[$i] == $sql_tovar['kolvo'] ) {
         $out = "<tr><td>".$sql_tovar['id']."</td><td>";
         $out = $out."<a href=product.php?id=".$sql_tovar['id'].">".$sql_tovar['name'];
         $out = $out."</a></td><td>".$sql_tovar['chena_output'];
-        $out = $out."</td><td><button onclick='minusCart(".$sql_tovar['id'].")' type='submit'>-</button>".$kolvo;
+        $out = $out."</td><td><button onclick='minusCart(".$sql_tovar['id'].")' type='submit'>-</button>".$kolvo[$i];
         $out = $out."шт.</td>";
-        $out = $out."<td><button onclick='removeCart(".$sql_tovar['id'].",".$kolvo.")' type='submit'>Убрать</button></td></tr>";
+        $out = $out."<td><button onclick='removeCart(".$sql_tovar['id'].",".$kolvo[$i].")' type='submit'>Убрать</button></td></tr>";
         echo $out;
     }
-    if ( $kolvo < $sql_tovar['kolvo'] ) {
+    if ( $kolvo[$i] < $sql_tovar['kolvo'] ) {
         $out = "<tr><td>".$sql_tovar['id']."</td><td>";
         $out = $out."<a href=product.php?id=".$sql_tovar['id'].">".$sql_tovar['name'];
         $out = $out."</a></td><td>".$sql_tovar['chena_output'];
-        $out = $out."</td><td><button onclick='minusCart(".$sql_tovar['id'].")' type='submit'>-</button>".$kolvo;
+        $out = $out."</td><td><button onclick='minusCart(".$sql_tovar['id'].")' type='submit'>-</button>".$kolvo[$i];
         $out = $out."<button onclick='plusCart(".$sql_tovar['id'].")' type='submit'>+</button></td>";
-        $out = $out."<td><button onclick='removeCart(".$sql_tovar['id'].",".$kolvo.")' type='submit'>Убрать</button></td></tr>";
+        $out = $out."<td><button onclick='removeCart(".$sql_tovar['id'].",".$kolvo[$i].")' type='submit'>Убрать</button></td></tr>";
         echo $out;
     }
+
 }
-?>
+    $i++;
+}
+
+//if ( $kolvo[$i] == '0' ) {?>
+<!--Нет ниодного товара в корзине-->
+<?// } ?>
