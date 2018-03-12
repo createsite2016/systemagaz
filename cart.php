d6b6431cebbfa0db48861b9c4d6aa043b0db1dfe
Andrey Galushko <createsite2016@gmail.com>
2018-03-13 02:14:25 +0300

    Изменил СЕО продвижение
    теперь на странице корзина указанно название магазина, город и корзина

--- cart.php
+++ cart.php
@@ -8,7 +8,7 @@ $template["TEMPLATE_PATH"] = 'theme/bf_vjx/'; // путь до макета ша
 $template["MAGAZIN"] = $pdo->getRow("SELECT * FROM `magazins`"); // данные о магазине
 
 $template["CATEGORIES"] = $pdo->getRows("SELECT `id`,`name` FROM `categor` ORDER BY `sort`"); // список категорий
-$template["NAME_OPEN_CATEGOR"]["name"] = 'корзина'; // имя активной страницы
+$template["NAME_OPEN_CATEGOR"]["name"] = $template["MAGAZIN"]['name']; // имя активной страницы
 
 if ( !empty($_SESSION["cart"]) ) {
     foreach ($_SESSION["cart"] as $key=>$value) { // получаю из сессии значения ключей и упорядочно кладу их в массив $ids
@@ -35,9 +35,9 @@ if ( !empty($_SESSION["cart"]) ) {
 
 
 $template["PAGES"] = $pdo->getRows("SELECT `name`,`id`,`about` FROM `pages` ORDER BY `id`"); // получение страниц пользователя
-$template["SEO"]["TITLE"] = $template["NAME_OPEN_CATEGOR"]['name'].' '.$template["MAGAZIN"]['city']; // Заголовок
-$template["SEO"]["KEYWORDS"] = $template["NAME_OPEN_CATEGOR"]['name'].' '.$template["MAGAZIN"]['city']; // Ключевые слова
-$template["SEO"]["DESCRIPTION"] = $template["NAME_OPEN_CATEGOR"]['name'].' '.$template["MAGAZIN"]['city']; // Описание
+$template["SEO"]["TITLE"] = $template["NAME_OPEN_CATEGOR"]['name'].' '.$template["MAGAZIN"]['city'].' - корзина'; // Заголовок
+$template["SEO"]["KEYWORDS"] = $template["NAME_OPEN_CATEGOR"]['name'].' '.$template["MAGAZIN"]['city'].' - корзина'; // Ключевые слова
+$template["SEO"]["DESCRIPTION"] = $template["NAME_OPEN_CATEGOR"]['name'].' '.$template["MAGAZIN"]['city'].' - корзина'; // Описание
 
 include $template["TEMPLATE_PATH"].'cart.php'; // подключение страницы шаблона
 ?>
